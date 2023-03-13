1a2c4ac86919a211359fa4db9cf3fb22bac751fd
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-03-13 16:29:13 +0000

    barebones

--- README.md
+++
@@ -1,2 +0,0 @@
-# todeldraw
-tldraw experiments
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/editor.css";
+import "@tldraw/tldraw/ui.css";
+
+export default function () {
+  return (
+    <div className="tldraw__editor">
+      <Tldraw />
+    </div>
+  );
+}
